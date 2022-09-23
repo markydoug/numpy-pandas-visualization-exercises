c6e2c1e7df30929dac5a534bb07819436c444c4c
Mark Foster <markdouglasfoster@gmail.com>
2022-09-22 21:44:02 -0500

    Add setup 1 and 2 exercises

--- numpy_exercises.py
+++ numpy_exercises.py
@@ -39,96 +39,153 @@ a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # Use python's built in functionality/operators to determine the following:
 # Exercise 1 - Make a variable called sum_of_a to hold the sum of all the numbers in above list
 sum_of_a = sum(a)
-sum_of_a #36
+sum_of_a  55
 
 # Exercise 2 - Make a variable named min_of_a to hold the minimum of all the numbers in the above list
 min_of_a = min(a)
-min_of_a # -7
+min_of_a # 1
 
 # Exercise 3 - Make a variable named max_of_a to hold the max number of all the numbers in the above list
 max_of_a = max(a)
-max_of_a #23
+max_of_a # 10
 
 # Exercise 4 - Make a variable named mean_of_a to hold the average of all the numbers in the above list
 mean_of_a = sum_of_a / len(a)
-mean_of_a #3
+mean_of_a # 3
 
 # Exercise 5 - Make a variable named product_of_a to hold the product of multiplying all the numbers in the above list together
 product_of_a = 1
+x = 0
 for x in a:
     product_of_a = product_of_a * x
 
+product_of_a # 3628800
 
 # Exercise 6 - Make a variable named squares_of_a. It should hold each number in a squared like [1, 4, 9, 16, 25...]
+square_of_a = []
+x = 0
+for x in a:
+    square_of_a.append(x**2)
 
 # Exercise 7 - Make a variable named odds_in_a. It should hold only the odd numbers
+odds_in_a = []
+for n in a:
+    if n % 2 == 1:
+        odds_in_a.append(n)
+
+odds_in_a
 
 # Exercise 8 - Make a variable named evens_in_a. It should hold only the evens.
+evens_in_a = []
+for n in a:
+    if n % 2 == 0:
+        evens_in_a.append(n)
+
+evens_in_a
+
 
 ## What about life in two dimensions? A list of lists is matrix, a table, a spreadsheet, a chessboard...
 ## Setup 2: Consider what it would take to find the sum, min, max, average, sum, product, and list of squares for this list of two lists.
-b = [
+b = np.array([
     [3, 4, 5],
     [6, 7, 8]
-]
+])
 
 # Exercise 1 - refactor the following to use numpy. Use sum_of_b as the variable. **Hint, you'll first need to make sure that the "b" variable is a numpy array**
-sum_of_b = 0
-for row in b:
-    sum_of_b += sum(row)
+#sum_of_b = 0
+#for row in b:
+#    sum_of_b += sum(row)
+
+sum_of_b = b.sum()
+# 33
 
 # Exercise 2 - refactor the following to use numpy. 
-min_of_b = min(b[0]) if min(b[0]) <= min(b[1]) else min(b[1])  
+#min_of_b = min(b[0]) if min(b[0]) <= min(b[1]) else min(b[1]) 
+
+min_of_b = b.min()
+# 3
 
 # Exercise 3 - refactor the following maximum calculation to find the answer with numpy.
-max_of_b = max(b[0]) if max(b[0]) >= max(b[1]) else max(b[1])
+#max_of_b = max(b[0]) if max(b[0]) >= max(b[1]) else max(b[1])
 
+max_of_b = b.max()
+ #8
 
 # Exercise 4 - refactor the following using numpy to find the mean of b
-mean_of_b = (sum(b[0]) + sum(b[1])) / (len(b[0]) + len(b[1]))
+# mean_of_b = (sum(b[0]) + sum(b[1])) / (len(b[0]) + len(b[1]))
+
+mean_of_b = b.mean()
+# 5.5
 
 # Exercise 5 - refactor the following to use numpy for calculating the product of all numbers multiplied together.
-product_of_b = 1
-for row in b:
-    for number in row:
-        product_of_b *= number
+# product_of_b = 1
+# for row in b:
+#    for number in row:
+#        product_of_b *= number
+
+product_of_b = b.prod()
+# 20160
 
 # Exercise 6 - refactor the following to use numpy to find the list of squares 
-squares_of_b = []
-for row in b:
-    for number in row:
-        squares_of_b.append(number**2)
+# squares_of_b = []
+# for row in b:
+#    for number in row:
+#        squares_of_b.append(number**2)
 
+squares_of_b = b**2
+# array([[ 9, 16, 25],
+#       [36, 49, 64]])
 
 # Exercise 7 - refactor using numpy to determine the odds_in_b
-odds_in_b = []
-for row in b:
-    for number in row:
-        if(number % 2 != 0):
-            odds_in_b.append(number)
+#odds_in_b = []
+#for row in b:
+#    for number in row:
+#        if(number % 2 != 0):
+#            odds_in_b.append(number)
 
+odds_in_b = b[b % 2 == 1]
+#array([3, 5, 7])
 
 # Exercise 8 - refactor the following to use numpy to filter only the even numbers
-evens_in_b = []
-for row in b:
-    for number in row:
-        if(number % 2 == 0):
-            evens_in_b.append(number)
+#evens_in_b = []
+#for row in b:
+#    for number in row:
+#        if(number % 2 == 0):
+#            evens_in_b.append(number)
+
+evens_in_b = b[b % 2 == 0]
+#array([4, 6, 8])
 
 # Exercise 9 - print out the shape of the array b.
+b.shape
+#(2, 3)
 
 # Exercise 10 - transpose the array b.
+b.T
+#array([[3, 6],
+#       [4, 7],
+#       [5, 8]])
 
 # Exercise 11 - reshape the array b to be a single list of 6 numbers. (1 x 6)
+b.reshape(1,6)
+# array([[3, 4, 5, 6, 7, 8]])
 
 # Exercise 12 - reshape the array b to be a list of 6 lists, each containing only 1 number (6 x 1)
+b.reshape(6,1)
+#array([[3],
+#       [4],
+#       [5],
+#       [6],
+3       [7],
+#       [8]])
+
 
 ## Setup 3
-c = [
+c = np.array([
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
-]
+])
 
 # HINT, you'll first need to make sure that the "c" variable is a numpy array prior to using numpy array methods.
 # Exercise 1 - Find the min, max, sum, and product of c.
