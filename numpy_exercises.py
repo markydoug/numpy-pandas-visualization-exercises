257ab4a4948c840c450142630feddc41af73c15a
Mark Foster <markdouglasfoster@gmail.com>
2022-09-23 09:18:43 -0500

    Add rest of the exercises

--- numpy_exercises.py
+++ numpy_exercises.py
@@ -189,45 +189,115 @@ c = np.array([
 
 # HINT, you'll first need to make sure that the "c" variable is a numpy array prior to using numpy array methods.
 # Exercise 1 - Find the min, max, sum, and product of c.
+c.min() # 1
+c.max() # 9
+c.sum() # 45
+c.prod() # 362880
 
 # Exercise 2 - Determine the standard deviation of c.
+c.std() # 2.581988897471611
 
 # Exercise 3 - Determine the variance of c.
+c.var() # .666666666666667
 
 # Exercise 4 - Print out the shape of the array c
+c.shape # (3, 3)
 
 # Exercise 5 - Transpose c and print out transposed result.
+c.T
+#array([[1, 4, 7],
+#       [2, 5, 8],
+#       [3, 6, 9]])
 
 # Exercise 6 - Get the dot product of the array c with c. 
+np.dot(c,c)
+#array([[ 30,  36,  42],
+#       [ 66,  81,  96],
+#       [102, 126, 150]])
 
 # Exercise 7 - Write the code necessary to sum up the result of c times c transposed. Answer should be 261
+(c * c.T).sum()
+#261
 
 # Exercise 8 - Write the code necessary to determine the product of c times c transposed. Answer should be 131681894400.
-
+(c * c.T).prod()
+#131681894400
 
 ## Setup 4
-d = [
+d = np.array([
     [90, 30, 45, 0, 120, 180],
     [45, -90, -30, 270, 90, 0],
     [60, 45, -45, 90, -45, 180]
-]
+])
 
 # Exercise 1 - Find the sine of all the numbers in d
+np.sin(d)
+'''
+array([[ 0.89399666, -0.98803162,  0.85090352,  0.        ,  0.58061118,
+        -0.80115264],
+       [ 0.85090352, -0.89399666,  0.98803162, -0.17604595,  0.89399666,
+         0.        ],
+       [-0.30481062,  0.85090352, -0.85090352,  0.89399666, -0.85090352,
+        -0.80115264]])
+'''
 
 # Exercise 2 - Find the cosine of all the numbers in d
+np.cos(d)
+'''
+array([[-0.44807362,  0.15425145,  0.52532199,  1.        ,  0.81418097,
+        -0.59846007],
+       [ 0.52532199, -0.44807362,  0.15425145,  0.98438195, -0.44807362,
+         1.        ],
+       [-0.95241298,  0.52532199,  0.52532199, -0.44807362,  0.52532199,
+        -0.59846007]])
+'''
 
 # Exercise 3 - Find the tangent of all the numbers in d
-
+np.tan(d)
+'''
+array([[-1.99520041, -6.4053312 ,  1.61977519,  0.        ,  0.71312301,
+         1.33869021],
+       [ 1.61977519,  1.99520041,  6.4053312 , -0.17883906, -1.99520041,
+         0.        ],
+       [ 0.32004039,  1.61977519, -1.61977519, -1.99520041, -1.61977519,
+         1.33869021]])
+'''
 # Exercise 4 - Find all the negative numbers in d
+d[d <0]
+# array([-90, -30, -45, -45])
 
 # Exercise 5 - Find all the positive numbers in d
+d[d >0]
+#array([ 90,  30,  45, 120, 180,  45, 270,  90,  60,  45,  90, 180])
 
 # Exercise 6 - Return an array of only the unique numbers in d.
+np.unique(d)
+#array([-90, -45, -30,   0,  30,  45,  60,  90, 120, 180, 270])
 
 # Exercise 7 - Determine how many unique numbers there are in d.
+len(np.unique(d))
+#11
 
 # Exercise 8 - Print out the shape of d.
+d.shape
+# (3, 6)
 
 # Exercise 9 - Transpose and then print out the shape of d.
+d.T.shape
+# (6, 3)
 
 # Exercise 10 - Reshape d into an array of 9 x 2
+d.reshape(9,2)
+'''
+array([[ 90,  30],
+       [ 45,   0],
+       [120, 180],
+       [ 45, -90],
+       [-30, 270],
+       [ 90,   0],
+       [ 60,  45],
+       [-45,  90],
+       [-45, 180]])
+'''
+
+
